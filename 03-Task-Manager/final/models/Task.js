3419a1246c33d35db3aabbbf693fb53f5a01f055
procoder0001 <weichen.1994.0613@gmail.com>
2024-07-17 04:03:42 +0900

    fix: 10 files in task manager file

--- 03-Task-Manager/final/models/Task.js
+++ 03-Task-Manager/final/models/Task.js
@@ -1,16 +1,16 @@
-const mongoose = require('mongoose')
+const mongoose = require("mongoose");
 
 const TaskSchema = new mongoose.Schema({
   name: {
     type: String,
-    required: [true, 'must provide name'],
+    required: [true, "must provide name"],
     trim: true,
-    maxlength: [20, 'name can not be more than 20 characters'],
+    maxlength: [20, "name can not be more than 20 characters"],
   },
   completed: {
     type: Boolean,
     default: false,
   },
-})
+});
 
-module.exports = mongoose.model('Task', TaskSchema)
+module.exports = mongoose.model("Task", TaskSchema);
--- 03-Task-Manager/final/routes/tasks.js
+++ 03-Task-Manager/final/routes/tasks.js
@@ -1,5 +1,5 @@
-const express = require('express')
-const router = express.Router()
+const express = require("express");
+const router = express.Router();
 
 const {
   getAllTasks,
@@ -8,9 +8,9 @@ const {
   updateTask,
   deleteTask,
   editTask,
-} = require('../controllers/tasks')
+} = require("../controllers/tasks");
 
-router.route('/').get(getAllTasks).post(createTask)
-router.route('/:id').get(getTask).patch(updateTask).delete(deleteTask)
+router.route("/").get(getAllTasks).post(createTask);
+router.route("/:id").get(getTask).patch(updateTask).delete(deleteTask);
 
-module.exports = router
+module.exports = router;
