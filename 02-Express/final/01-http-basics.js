d9576a6ebc5db95a405cb8258d80a0b0f6812d17
coderprovider <white.pury1114@gmail.com>
2024-07-22 23:39:11 +0900

    fix: dash files

--- 02-Express/final/01-http-basics.js
+++ 02-Express/final/01-http-basics.js
@@ -1,30 +1,30 @@
-const http = require('http');
-const { readFileSync } = require('fs');
+const http = require("http");
+const { readFileSync } = require("fs");
 
 // get all files
-const homePage = readFileSync('../navbar-app/index.html');
+const homePage = readFileSync("../navbar-app/index.html");
 
 const server = http.createServer((req, res) => {
-    // console.log(req.method);
-    const url = req.url;
-    // home page
-    if (url === '/') {
-        res.writeHead(200, { 'content-type': 'text/html' });
-        res.write(homePage);
-        res.end();
-    }
-    // about page 
-    else if (url === '/about') {
-        res.writeHead(200, { 'content-type': 'text/html' });
-        res.write('<h1>about page</h1>');
-        res.end();
-    }
-    // 404
-    else {
-        res.writeHead(404, { 'content-type': 'text/html' });
-        res.write('<h1>page not found</h1>');
-        res.end();
-    }
+  // console.log(req.method);
+  const url = req.url;
+  // home page
+  if (url === "/") {
+    res.writeHead(200, { "content-type": "text/html" });
+    res.write(homePage);
+    res.end();
+  }
+  // about page
+  else if (url === "/about") {
+    res.writeHead(200, { "content-type": "text/html" });
+    res.write("<h1>about page</h1>");
+    res.end();
+  }
+  // 404
+  else {
+    res.writeHead(404, { "content-type": "text/html" });
+    res.write("<h1>page not found</h1>");
+    res.end();
+  }
 });
 
 server.listen(5000);
--- 02-Express/final/03-express-basics.js
+++ 02-Express/final/03-express-basics.js
@@ -1,22 +1,22 @@
-const express = require('express');
+const express = require("express");
 const app = express();
 
-app.get('/', (req, res) => {
-    console.log('User hit the resource');
-    res.status(200).send('Home Page');
+app.get("/", (req, res) => {
+  console.log("User hit the resource");
+  res.status(200).send("Home Page");
 });
 
-app.get('/about', (req, res) => {
-    res.status(200).send('About Page');
+app.get("/about", (req, res) => {
+  res.status(200).send("About Page");
 });
 
-app.all('*', (req, res) => {
-    res.status(404).send('<h1>Resource not found</h1>');
+app.all("*", (req, res) => {
+  res.status(404).send("<h1>Resource not found</h1>");
 });
 
 const port = 5000;
 app.listen(port, () => {
-    console.log(`Server is listening on port ${port}`);
+  console.log(`Server is listening on port ${port}`);
 });
 
 // --> Call Methods:
